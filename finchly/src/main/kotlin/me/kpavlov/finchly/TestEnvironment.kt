3a86b8d895a917700cbe6515c197bb5e485775af
Konstantin Pavlov <{ID}+{username}@users.noreply.github.com>
2024-11-09 17:35:41 +0200

    Add finchly module and configure environment support
    Introduce a new 'finchly' module to the project. The module includes a TestEnvironment class leveraging dotenv for environment configuration and SLF4J for logging. Additionally, update the parent POM with necessary dependencies and correct the sequence of modules.

---
+++ finchly/src/main/kotlin/me/kpavlov/finchly/TestEnvironment.kt
@@ -0,0 +1,18 @@
+package me.kpavlov.finchly
+
+import io.github.cdimascio.dotenv.dotenv
+import org.slf4j.LoggerFactory
+import java.nio.file.Paths
+
+private val logger = LoggerFactory.getLogger(TestEnvironment.javaClass)
+
+object TestEnvironment {
+    private val dotenv =
+        dotenv {
+            directory = Paths.get("${System.getProperty("user.dir")}/..").normalize().toString()
+            logger.info("Loading .env file from $directory")
+            ignoreIfMissing = true
+        }
+
+    fun env(name: String) = dotenv.get(name)
+}
